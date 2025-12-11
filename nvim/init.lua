0c2d25003f2431d34148f7661c5df96c311c75ad
Praveen Perumal <ppraveen98841@gmail.com>
2025-12-11 08:38:09 +0530

    Added support markdown in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,11 +40,15 @@ MiniDeps.add({ name = 'mini.nvim', checkout = 'stable' })
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 later(function()
+	add({ source = 'smoka7/hop.nvim' })
+	local hop = require('hop')
+	hop.setup({
+		case_insensitive = false,
+		multi_windows = true,
+	})
 	require('mini.comment').setup()
 	require('mini.pairs').setup()
 	require('mini.operators').setup()
-
-	-- To make mini.surround behavior like tpope/vim-surround
     require('mini.surround').setup({
       mappings = {
         add = 'ys',
@@ -62,23 +66,19 @@ later(function()
 	vim.keymap.del('x', 'ys')
 	vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
 	vim.keymap.set('n', 'yss', 'ys_', { remap = true })
+	vim.keymap.set({'n', 'o', 'x'}, 'm', hop.hint_char1, { desc = "Move curosr", remap = false })
+	vim.keymap.set('n', '<leader>m', 'm', { desc = "Set mark" })
+	vim.keymap.set('o', '%', '<cmd>normal!ggVG<cr>', { silent = true, remap = false })
 end)
 
 later(function()
-	add({ source = 'smoka7/hop.nvim' })
-	local hop = require('hop')
-	hop.setup({
-		case_insensitive = false,
-		multi_windows = true,
-	})
-	vim.keymap.set({'n', 'o', 'x'}, 'm', hop.hint_char1, { desc = "Move curosr", remap = false })
-	vim.keymap.set('n', '<leader>m', 'm', { desc = "Set mark" })
 end)
 
 if vim.g.vscode then
 	local vscode = require("vscode")
 	later(function()
 		-- Enable basic mapping only
+		-- `gy` and `gp` for system clipboard
 		require('mini.basics').setup({
 			options = { basic = false },
 			mappings = { 
@@ -93,28 +93,47 @@ else
 	now(function()
 		add({ source = 'ellisonleao/gruvbox.nvim' })
 
-		local gruvbox = require('gruvbox')
-		gruvbox.setup({
+		require('gruvbox').setup({
 			transparent_mode = true
 		})
-		vim.o.termguicolors = true
-		vim.cmd('colorscheme gruvbox')
-
-		vim.keymap.set('n', '<C-b>', '<C-^>', { silent = true })
-		vim.keymap.set('n', '<C-c>', '<cmd>bd<cr>', { silent = true })
-		vim.keymap.set('n', '<C-C>', '<cmd>bd!<cr>', { silent = true })
-	end)
-
-	now(function()
 		require('mini.tabline').setup({
 			use_icons = false
 		})
 		require('mini.statusline').setup({
 			use_icons = false
 		})
+		require('mini.files').setup({
+			use_icons = false,
+			content = { prefix = function() end, },
+			windows = {
+				max_number = 3,
+			},
+			options = {
+				use_as_default_explorer = true,
+			},
+		})
+
+		vim.o.hlsearch = false
+		vim.o.termguicolors = true
+		vim.cmd('colorscheme gruvbox')
+
+		vim.keymap.set('n', '<C-b>', '<C-^>', { silent = true })
+		vim.keymap.set('n', '<C-c>', '<cmd>bd<cr>', { silent = true })
+		vim.keymap.set('n', '<C-C>', '<cmd>bd!<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>n', MiniFiles.open, { desc = "Open MiniFiles" })
 	end)
 
 	later(function()
+		require('mini.bracketed').setup()
+		require('mini.basics').setup({
+			options = { basic = false },
+			mappings = { 
+				basic = true,
+				option_toggle_prefix = '<leader>t',
+				windows = true,
+			},
+			autocommands = { basic = true },
+		})
 		local miniclue = require('mini.clue')
 		miniclue.setup({
 			triggers = {
@@ -135,50 +154,37 @@ else
 		})
 	end)
 
-	later(function()
-		require('mini.bracketed').setup()
-		require('mini.basics').setup({
-			options = { basic = false },
-			mappings = { 
-				basic = true,
-				option_toggle_prefix = '<leader>t',
-				windows = true,
-			},
-			autocommands = { basic = true },
-		})
-	end)
+	-- later(function()
+	-- 	add({ source = 'arnamak/stay-centered.nvim' })
+	--
+	-- 	local stay_centered = require('stay-centered')
+	-- 	stay_centered.setup()
+	--
+	-- 	vim.keymap.set('n', '<leader>tz', stay_centered.toggle, { desc = "Toggle 'stay_centered'"})
+	-- end)
 
-	later(function()
-		add({ source = 'arnamak/stay-centered.nvim' })
-
-		local stay_centered = require('stay-centered')
-		stay_centered.setup()
-
-		vim.keymap.set('n', '<leader>tz', stay_centered.toggle, { desc = "Toggle 'stay_centered'"})
-	end)
-
-	later(function()
-		local function toggle_unnamedplus()
-			local current_clip = vim.opt.clipboard:get()
-
-			local found = false
-			for _, item in ipairs(current_clip) do
-				if item == 'unnamedplus' then
-					found = true
-					break
-				end
-			end
-
-			if found then
-				vim.opt.clipboard:remove("unnamedplus")
-				vim.notify("System Clipboard: OFF)", vim.log.levels.INFO)
-			else
-				vim.opt.clipboard:append("unnamedplus")
-				vim.notify("System Clipboard: ON", vim.log.levels.INFO)
-			end
-		end
-		vim.keymap.set('n', '<leader>ty', toggle_unnamedplus, { desc = "Toggle 'clipboard'"})
-	end)
+	-- later(function()
+	-- 	local function toggle_unnamedplus()
+	-- 		local current_clip = vim.opt.clipboard:get()
+	--
+	-- 		local found = false
+	-- 		for _, item in ipairs(current_clip) do
+	-- 			if item == 'unnamedplus' then
+	-- 				found = true
+	-- 				break
+	-- 			end
+	-- 		end
+	--
+	-- 		if found then
+	-- 			vim.opt.clipboard:remove("unnamedplus")
+	-- 			vim.notify("System Clipboard: OFF)", vim.log.levels.INFO)
+	-- 		else
+	-- 			vim.opt.clipboard:append("unnamedplus")
+	-- 			vim.notify("System Clipboard: ON", vim.log.levels.INFO)
+	-- 		end
+	-- 	end
+	-- 	vim.keymap.set('n', '<leader>ty', toggle_unnamedplus, { desc = "Toggle 'clipboard'"})
+	-- end)
 
 	later(function()
 		add({
@@ -190,8 +196,11 @@ else
 			end },
 		})
 
-		local treesitter = require('nvim-treesitter.configs')
-		treesitter.setup({
+		require('nvim-treesitter.configs').setup({
+			ensure_installed = { "markdown", "markdown_inline" },
+			markdown = {
+				enable = true,
+			},
 			auto_install = true,
 			ignore_install = {
 				'tmux',
@@ -203,16 +212,6 @@ else
 		})
 	end)
 
-	later(function()
-		require('mini.files').setup({
-			use_icons = false,
-			content = { prefix = function() end, },
-			windows = {
-				max_number = 3,
-			},
-		})
-		vim.keymap.set('n', '<leader>n', MiniFiles.open, { desc = "Open MiniFiles" })
-	end)
 
 	later(function()
 		local pick = require('mini.pick')
@@ -246,5 +245,62 @@ else
 			end
 		end
 		vim.keymap.set('i', '<C-y>', 'v:lua.ctrl_y_action()', { expr = true })
+	end)
+
+	later(function()
+		add({ source = 'tadmccorkle/markdown.nvim' })
+
+		-- concealed text is completely hidden unless
+		-- it has a custom replacement character defined.
+		vim.o.conceallevel = 2
+		vim.o.concealcursor = ""
+		vim.o.foldmethod = "marker"
+
+		require('markdown').setup({
+			mappings = {
+				inline_surround_toggle = "ys",
+				inline_surround_toggle_line = "yss",
+				inline_surround_delete = "ds",
+				inline_surround_change = "cs",
+				link_add = "ga",
+				link_follow = "gf",
+				go_curr_heading = false,
+				go_parent_heading = false,
+				go_next_heading = "]s",
+				go_prev_heading = "[s",
+			},
+			on_attach = function(bufnr)
+				local map = vim.keymap.set
+				local opts = { buffer = bufnr }
+				map('n', 'go', '<Cmd>MDListItemBelow<CR>', opts)
+				map('n', 'gO', '<Cmd>MDListItemAbove<CR>', opts)
+			end,
+		})
+
+		-- :New command creates a new markdown file from existing TEMPLATE.md
+		-- file in the current working directory
+		vim.api.nvim_create_user_command(
+			'New',
+			function(opts)
+				local fname = opts.fargs[1] .. '.md'
+				local exist = vim.system({"test", "-f", fname}, {text=true}):wait()
+				if exist.code == 0 then
+					-- if file exists then open
+					vim.cmd("edit " .. fname)
+				else
+					-- otherwise, create the file and then open
+					local status = vim.system({"cp", "TEMPLATE.md", fname}, {text=true}):wait()
+					if status.code == 0 then
+						-- if success then open
+						vim.cmd("edit " .. fname)
+					else
+						-- otherwise, echo
+						vim.cmd('echo "Failed"')
+					end
+				end
+
+			end,
+			{ nargs = 1 }
+		)
 	end)
 end
